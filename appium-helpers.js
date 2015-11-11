9dd33b83f56794c91307005b25a86aa57305746a
Jon Appelberg <jon@unity3d.com>
2015-11-11 15:54:49 +0200

    Intial lib upload

---
+++ appium-helpers.js
@@ -0,0 +1,72 @@
+/* jshint node: true, multistr: true */
+
+// The buttons on the ad screen cannot be recognized by same appium methods
+// on iOS as on Android, Use this method instead.
+var staticTextElement = function(staticText, timeOut, pollInterval) {
+  if(process.env.APPIUM_PLATFORM == "ios"){
+    return this
+      .waitForElementByIosUIAutomation('.scrollViews()[0]\
+                                        .webViews()[0]\
+                                        .staticTexts()["' + staticText+ '"]',
+                                        timeOut, pollInterval);
+  } else {
+    return this
+      .waitForElementByName(staticText, timeOut, pollInterval);
+  }
+};
+
+// Collect certain messages from log
+var messagesFromLog = function(logType, regexFilter) {
+  return this
+    .log(logType)
+    .then(function(logBuffer) {
+      var entries = logBuffer.filter(function(entry) {
+        return regexFilter.test(entry.message);
+      });
+      return entries;
+    });
+};
+
+var jsonObjectsFromLog = function(logType, generalFilter, arrayOfFilterStrings) {
+  var jsonObjects = {};
+  return this
+    .messagesFromLog(logType, generalFilter)
+    .then(function(entries) {
+      for( var i = 0 ; i < arrayOfFilterStrings.length ; i++ ) {
+        var filterStr = arrayOfFilterStrings[i];
+        var regex = new RegExp(filterStr);
+        var messages = entries.filter(function(entry) {
+          return regex.test(entry.message);
+        });
+        var lastMessage = messages[ messages.length -1 ];
+        if(!lastMessage){
+          throw new Error("No such message found! '" + filterStr + "'");
+        }
+        jsonObjects[filterStr] = parseJsonFromMessage(messages[ messages.length -1 ].message);
+      }
+      return jsonObjects;
+    });
+};
+
+var logContexts = function(tagStr){
+  return this
+    .contexts()
+    .then(function(contexts){
+      console.log("Visible contexts at " + tagStr + ": " + contexts);
+      return this;
+    });
+}
+
+exports.configureWd = function(wd) {
+  wd.addPromiseChainMethod('staticTextElement', staticTextElement);
+  wd.addPromiseChainMethod('messagesFromLog', messagesFromLog);
+  wd.addPromiseChainMethod('jsonObjectsFromLog', jsonObjectsFromLog);
+  wd.addPromiseChainMethod('logContexts', logContexts);
+};
+
+exports.configureYiewdDriver = function(driver){
+  driver.staticTextElement = staticTextElement;
+  driver.messagesFromLog = messagesFromLog;
+  driver.jsonObjectsFromLog = jsonObjectsFromLog;
+  driver.logContexts = logContexts;
+};
---
+++ test_start_helpers.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Helpers for commandline scripts related to appium tests.
+
+function get_full_path {
+  echo "$( cd "$(dirname "$1")"; echo "$(pwd)/$(basename "$1")" )"
+}
+
+function android_reboot_and_wait_for_device_ready {
+  echo "Rebooting android device"
+  adb reboot
+  sleep 5
+  adb wait-for-device
+  while [ -z "$(adb shell getprop sys.boot_completed | tr -d '\r')" ]; do
+    sleep 1
+    printf "_"
+  done
+  until [ "$(adb shell getprop sys.boot_completed | tr -d '\r')" -eq 1 ]; do
+    sleep 1
+    print "."
+  done
+  echo
+  echo "Rebooted"
+}
+
+function start_script {
+  npm install chai@2.1.2 colors underscore chai-as-promised wd path mkdirp yiewd tail mocha mocha-junit-reporter
+  echo "mocha executable: '$(file node_modules/.bin/mocha)'"
+  MOCHA_BIN='./node_modules/.bin/mocha'
+  
+  echo "Running tests '$TEST'"
+  if [ "$TESTDROID" -eq "1" ]; then
+    ${MOCHA_BIN} "${TEST}" --reporter mocha-junit-reporter --reporter-options mochaFile=./TEST-all.xml 2>&1
+  else
+    ${MOCHA_BIN} "$TEST"
+  fi
+  return $?
+}
