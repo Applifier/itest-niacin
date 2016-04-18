64aea773de80e93751d8f5c0463be8d4ac1a916a
Jon Appelberg <jon@unity3d.com>
2016-04-18 16:56:22 +0300

    Added support to periodically fetch a url and output response in logcat. Can be used to verify that backend is running and reachable throughout the tests

--- logcat_pinger.sh
+++ logcat_pinger.sh
@@ -24,6 +24,32 @@ function ping_to_logcat {
   ) &
 }
 
+# curl_to_logcat
+# Will make device http get the url given as argument and ouput result live
+# in logcat. Will do http get using curl until stop_ping_to_logcat() is called
+# Will return instantly and do the http get in subshell.
+# Args:
+# $1 url_to_fetch
+function curl_to_logcat {
+  MINIMUM_LOOP_TIME=10
+  curler_cmd="log -pd -tQAcurler \$(curl -s -m 2 \"$1\" || echo -n "FAILED")"
+  echo "Will loop command '$curler_cmd' on device"
+  touch "${TEST_RUNNING_FILE:?}"
+  (
+    while [ -f "${TEST_RUNNING_FILE:?}" ]
+    do
+      date_at_start=$(date +%s)
+      adb shell "$curler_cmd"
+      sleep 10
+      # Avoid looping out of control if something fails
+      if [ "$(( date_at_start + MINIMUM_LOOP_TIME))" -gt "$(date +%s)" ]; then
+        echo "Looping too fast, probably something failed. Throttling"
+        sleep 5
+      fi
+    done
+  ) &
+}
+
 # stop_ping_to_logcat
 # Stops the logging of pings to logcat. See ping_to_logcat()
 # Args:
