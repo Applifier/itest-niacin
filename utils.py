92ec4481ab38f5210c5a986af532d898785d1af6
Samuel Husso <samuel@unity3d.com>
2016-06-01 10:13:31 +0300

    added gitignore, utils.py

---
+++ utils.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from inspect import stack
+
+def function_name():
+    return stack()[1][3]
+
+def timing(f):
+    """
+    Decorator to trace the duration of a function call
+    Usage:
+        @timing
+        def my_method(some_args): do_something()
+    """
+    def wrap(*args):
+        time1 = time.time()
+        ret = f(*args)
+        time2 = time.time()
+        print("'%s' function took %0.3f ms to execute" % (f.__name__, (time2-time1)*1000.0))
+        return ret
+    return wrap
