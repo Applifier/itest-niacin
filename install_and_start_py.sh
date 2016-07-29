ddd804bd8c015cea42b8e109e6b722e0a7bda367
Samuel Husso <samuel@unity3d.com>
2016-07-29 09:02:30 +0300

    env variables to override python3 defaults

--- install_and_start_py.sh
+++ install_and_start_py.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+PYTHON=${PYTHON:=python3}
+PIP=${PIP:=pip3}
+
 function setup_virtualenv {
     virtualenv -p /usr/local/bin/python3 --no-site-packages --distribute .env
 }
@@ -15,24 +18,26 @@ function venv_activation {
 function install_libs_py {
     echo "installing py libs"
     if [ "$TESTDROID" == "1" ]; then
-        brew install ios-webkit-debug-proxy python3
+        # TODO: after TestDroid fixes their env lets not use python3
+        brew install ios-webkit-debug-proxy #python3
+        sudo easy_install pip
         export JUNIT_REPORT_STACK=1
         export JUNIT_REPORT_PATH="TEST-all.xml"
         JUNIT_REPORT_NAME="$DEVICE_NAME"
         JUNIT_REPORT_NAME=${JUNIT_REPORT_NAME:="No Device Name Set"}
         export JUNIT_REPORT_NAME
 
-        pip3 install -r "$SCRIPT_DIR/requirements.txt"
+        sudo $PIP install -r "$SCRIPT_DIR/requirements.txt"
     else
         # install python-packages inside the VirtualEnvironment
         venv_activation
-        pip3 install -r "$SCRIPT_DIR/requirements.txt"
+        $PIP install -r "$SCRIPT_DIR/requirements.txt"
     fi
 }
 
 function start_tests_py {
     echo "Running tests '$TEST'"
-    python3 "$TEST"
+    $PYTHON "$TEST"
     return $?
 }
 
