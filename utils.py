33b59e08234e7694587d20a6401d235faf801240
Samuel Husso <shusso@iki.fi>
2016-10-04 16:19:16 +0300

    generate random string, create object stubs

--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from inspect import stack
 from time import time
+import string
+import random
+
 
 def function_name():
     return stack()[1][3]
+
 
 def timing(f):
     """
@@ -19,3 +23,22 @@ def timing(f):
         print("'%s' function took %0.3f ms to execute" % (f.__name__, (time2-time1)*1000.0))
         return ret
     return wrap
+
+
+def object_creator(name):
+    """
+    Return new class stubb
+
+    Usage:
+        stub = object_creator(mystub)
+        stub.var = "some string"
+    """
+    return type(name.capitalize(), (object,), {})
+
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    """
+    Return a random string
+    http://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits-in-python/23728630#23728630
+    """
+    return ''.join(random.SystemRandom().choice(chars) for _ in range(size))
